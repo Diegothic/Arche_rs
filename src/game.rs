be8c4e3b06174a62435613aa1d84172c0b280583
Diegothic <diegothic24@gmail.com>
2023-03-07 20:19:57 +0100

    Archer Base

--- src/game.rs
+++ src/game.rs
@@ -1,36 +1,30 @@
-use bevy::{
-    prelude::*, render::camera::ScalingMode, sprite::MaterialMesh2dBundle, time::FixedTimestep,
-};
+use bevy::{prelude::*, render::camera::ScalingMode};
+
+use self::archer::{Archer, ArcherElement, ArcherPlugin, Bow, TrajectoryReceiver};
+
+mod archer;
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup)
-            .add_system_set(
-                SystemSet::new()
-                    .with_run_criteria(FixedTimestep::step(1.0 / 60.0))
-                    .with_system(rotate_system),
-            )
-            .add_system_set(
-                SystemSet::new()
-                    .with_system(bow_system)
-                    .with_system(bow_angle_system)
-                    .with_system(bow_pull_system)
-                    .with_system(trajectory_system)
-                    .with_system(trajectory_location_system)
-                    .with_system(trajectory_points_system.after(trajectory_system)),
-            );
+        app.add_plugin(ArcherPlugin)
+            .add_startup_system(setup_camera)
+            .add_startup_system(setup_trajectory)
+            .add_system(trajectory_system)
+            .add_system(trajectory_points_system);
     }
 }
 
-#[derive(Component)]
-struct Rotate;
-
-fn rotate_system(mut query: Query<&mut Transform, With<Rotate>>) {
-    for mut transform in query.iter_mut() {
-        transform.rotate_z(f32::to_radians(360.0) * 1.0 / 60.0);
-    }
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle {
+        projection: OrthographicProjection {
+            scaling_mode: ScalingMode::FixedVertical(9.0),
+            scale: 1.0,
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
 
 #[derive(Component)]
@@ -42,22 +36,25 @@ struct Trajectory {
 #[derive(Component)]
 struct TrajectoryPoint;
 
-fn trajectory_system(mut query: Query<&mut Trajectory>, query_bow: Query<&Bow>) {
-    if let Ok(bow) = query_bow.get_single() {
-        if let Ok(mut trajectory) = query.get_single_mut() {
-            trajectory.angle = bow.angle;
-            trajectory.power = bow.pull * 21.0 + 4.0;
-        }
-    }
-}
-
-fn trajectory_location_system(
-    mut query: Query<&mut Transform, With<Trajectory>>,
-    query_shooting_point: Query<&GlobalTransform, (With<ShootingPoint>, Without<Trajectory>)>,
+fn trajectory_system(
+    mut query: Query<(&mut Transform, &mut Trajectory)>,
+    query_archer: Query<&Archer, With<TrajectoryReceiver>>,
+    query_bow: Query<(&GlobalTransform, &ArcherElement), With<Bow>>,
 ) {
-    if let Ok(shooting_point_transform) = query_shooting_point.get_single() {
-        if let Ok(mut transform) = query.get_single_mut() {
-            transform.translation = shooting_point_transform.translation();
+    if let Ok((mut transform, mut trajectory)) = query.get_single_mut() {
+        if let Ok(archer) = query_archer.get_single() {
+            let mut found_transform: Option<&GlobalTransform> = Option::None;
+            for (transform, element) in query_bow.iter() {
+                if element.archer_id == archer.id {
+                    found_transform = Some(transform);
+                }
+            }
+
+            if let Some(bow_transform) = found_transform {
+                transform.translation = bow_transform.translation();
+                trajectory.angle = archer.aim_angle;
+                trajectory.power = archer.bow_pull * 28.0 + 2.0;
+            }
         }
     }
 }
@@ -68,7 +65,7 @@ fn trajectory_points_system(
 ) {
     if let Ok(trajectory) = query_trajectory.get_single() {
         let mut t: f32 = 0.0;
-        let t_delta = 1.0 / (trajectory.power * 2.0);
+        let t_delta = 1.0 / (trajectory.power * 10.0);
         for mut transform in query.iter_mut() {
             let x: f32 = trajectory.power * t * f32::cos(trajectory.angle);
             let mut y: f32 = trajectory.power * t * f32::sin(trajectory.angle);
@@ -77,106 +74,13 @@ fn trajectory_points_system(
             transform.translation.x = x;
             transform.translation.y = y;
             transform.translation.z = 5.0;
-            transform.scale = Vec3::splat(((1.0 - (t / t_delta / 50.0)) / 10.0) + 0.01);
+            transform.scale = Vec3::splat(((1.0 - (t / t_delta / 50.0)) / 20.0) + 0.01);
             t += t_delta;
         }
     }
 }
 
-#[derive(Component)]
-struct ShootingPoint;
-
-#[derive(Component)]
-struct Bow {
-    angle: f32,
-    pull: f32,
-}
-
-impl Default for Bow {
-    fn default() -> Self {
-        Self::new(0.0, 0.0)
-    }
-}
-
-impl Bow {
-    fn new(angle: f32, pull: f32) -> Self {
-        Self { angle, pull }
-    }
-
-    fn set_pull(&mut self, value: f32) {
-        self.pull = f32::clamp(value, 0.0, 1.0);
-    }
-}
-
-#[derive(Component)]
-struct StringPoint {
-    offset: f32,
-}
-
-impl Default for StringPoint {
-    fn default() -> Self {
-        Self::new(1.0)
-    }
-}
-
-impl StringPoint {
-    fn new(offset: f32) -> Self {
-        Self { offset }
-    }
-}
-
-fn bow_system(time: Res<Time>, keyboard: Res<Input<KeyCode>>, mut query: Query<&mut Bow>) {
-    if let Ok(mut bow) = query.get_single_mut() {
-        if keyboard.pressed(KeyCode::Up) {
-            bow.angle += 5.0 * time.delta_seconds();
-        }
-        if keyboard.pressed(KeyCode::Down) {
-            bow.angle -= 5.0 * time.delta_seconds();
-        }
-
-        if keyboard.pressed(KeyCode::Left) {
-            let new_pull = bow.pull + 2.0 * time.delta_seconds();
-            bow.set_pull(new_pull);
-        }
-        if keyboard.pressed(KeyCode::Right) {
-            let new_pull = bow.pull - 2.0 * time.delta_seconds();
-            bow.set_pull(new_pull);
-        }
-    }
-}
-
-fn bow_angle_system(mut query: Query<(&mut Transform, &Bow)>) {
-    if let Ok((mut transform, bow)) = query.get_single_mut() {
-        transform.rotation = Quat::from_euler(EulerRot::XYZ, 0.0, 0.0, bow.angle);
-    }
-}
-
-fn bow_pull_system(
-    mut query: Query<(&Parent, &StringPoint, &mut Transform)>,
-    query_parent: Query<&Bow>,
-) {
-    for (parent, string_point, mut transform) in query.iter_mut() {
-        if let Ok(parent_bow) = query_parent.get(parent.get()) {
-            let offset = string_point.offset * parent_bow.pull;
-            transform.translation.x = -offset;
-        }
-    }
-}
-
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    commands.spawn(Camera2dBundle {
-        projection: OrthographicProjection {
-            scaling_mode: ScalingMode::FixedVertical(9.0),
-            scale: 2.0,
-            ..Default::default()
-        },
-        ..Default::default()
-    });
-
+fn setup_trajectory(mut commands: Commands) {
     commands
         .spawn(SpatialBundle::default())
         .insert(Trajectory {
@@ -186,54 +90,11 @@ fn setup(
         .with_children(|parent| {
             for _ in 0..=50 {
                 parent
-                    .spawn(MaterialMesh2dBundle {
-                        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
-                        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0))
-                            .with_scale(Vec3::splat(0.3)),
-                        material: materials.add(ColorMaterial::from(Color::WHITE)),
+                    .spawn(SpriteBundle {
+                        transform: Transform::from_scale(Vec3::splat(0.3)),
                         ..Default::default()
                     })
                     .insert(TrajectoryPoint);
             }
         });
-
-    commands
-        .spawn(SpatialBundle::default())
-        .with_children(|parent| {
-            parent.spawn(MaterialMesh2dBundle {
-                mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
-                transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0))
-                    .with_scale(Vec3::new(1.0, 1.5, 1.0)),
-                material: materials.add(ColorMaterial::from(Color::BLUE)),
-                ..Default::default()
-            });
-            parent
-                .spawn(SpatialBundle {
-                    transform: Transform::from_translation(Vec3::new(0.5, 0.0, 0.0)),
-                    ..Default::default()
-                })
-                .with_children(|parent| {
-                    parent.spawn(MaterialMesh2dBundle {
-                        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
-                        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 1.0))
-                            .with_scale(Vec3::splat(0.1)),
-                        material: materials.add(ColorMaterial::from(Color::RED)),
-                        ..Default::default()
-                    });
-                })
-                .insert(ShootingPoint);
-            parent
-                .spawn(SpatialBundle::default())
-                .with_children(|parent| {
-                    parent.spawn(MaterialMesh2dBundle {
-                        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
-                        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 1.0))
-                            .with_scale(Vec3::splat(0.1)),
-                        material: materials.add(ColorMaterial::from(Color::RED)),
-                        ..Default::default()
-                    });
-                })
-                .insert(StringPoint { offset: 0.5 });
-        })
-        .insert(Bow::default());
 }
---
+++ src/game/archer.rs
@@ -0,0 +1,227 @@
+use std::f32::consts::PI;
+
+use bevy::{prelude::*, sprite::Anchor};
+
+pub struct ArcherPlugin;
+
+impl Plugin for ArcherPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(spawn_archer_system)
+            .add_system(target_rotation_system)
+            .add_system(archer_controll)
+            .add_system(archer_combat_limb_system);
+    }
+}
+
+#[derive(Component)]
+pub struct Archer {
+    pub id: u32,
+    pub aim_angle: f32,
+    pub bow_pull: f32,
+}
+
+#[derive(Component)]
+pub struct ArcherElement {
+    pub archer_id: u32,
+}
+
+#[derive(Component)]
+pub struct CombatLimb;
+
+#[derive(Component)]
+pub struct Bow;
+
+pub enum TargetRotationType {
+    Instant,
+    Smooth(f32),
+}
+
+#[derive(Component)]
+pub struct TargetRotation {
+    pub is_enabled: bool,
+    pub angle: f32,
+    pub rotation_type: TargetRotationType,
+}
+
+#[derive(Component)]
+pub struct TrajectoryReceiver;
+
+fn spawn_archer_system(mut commands: Commands) {
+    commands
+        .spawn(SpatialBundle {
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            // Right Arm
+            parent
+                .spawn(SpriteBundle {
+                    sprite: Sprite {
+                        color: Color::rgb(0.8, 0.8, 0.8),
+                        custom_size: Vec2::new(0.7, 0.2).into(),
+                        anchor: Anchor::CenterLeft,
+                        ..Default::default()
+                    },
+                    transform: Transform::from_translation(Vec3::new(0.25, 1.5, 1.0)),
+                    ..Default::default()
+                })
+                .with_children(|parent| {
+                    parent
+                        .spawn(SpriteBundle {
+                            sprite: Sprite {
+                                color: Color::rgb(0.2, 0.2, 1.0),
+                                custom_size: Vec2::new(0.25, 1.0).into(),
+                                anchor: Anchor::CenterRight,
+                                ..Default::default()
+                            },
+                            transform: Transform::from_translation(Vec3::new(0.7, 0.0, 10.0)),
+                            ..Default::default()
+                        })
+                        .insert((ArcherElement { archer_id: 0 }, Bow));
+                })
+                .insert((
+                    ArcherElement { archer_id: 0 },
+                    CombatLimb,
+                    TargetRotation {
+                        is_enabled: true,
+                        angle: 0.0,
+                        rotation_type: TargetRotationType::Smooth(4.0),
+                    },
+                ));
+
+            // Left Arm
+            parent.spawn(SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgb(0.8, 0.8, 0.8),
+                    custom_size: Vec2::new(0.7, 0.2).into(),
+                    anchor: Anchor::CenterRight,
+                    ..Default::default()
+                },
+                transform: Transform::from_translation(Vec3::new(-0.25, 1.5, 2.0)),
+                ..Default::default()
+            });
+
+            // Head
+            parent.spawn(SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgb(0.9, 0.9, 0.9),
+                    custom_size: Vec2::new(0.3, 0.3).into(),
+                    ..Default::default()
+                },
+                transform: Transform::from_translation(Vec3::new(0.0, 1.65, 0.0)),
+                ..Default::default()
+            });
+
+            // Torso
+            parent.spawn(SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgb(0.7, 0.7, 0.7),
+                    custom_size: Vec2::new(0.5, 0.7).into(),
+                    ..Default::default()
+                },
+                transform: Transform::from_translation(Vec3::new(0.0, 1.15, 0.0)),
+                ..Default::default()
+            });
+
+            // Legs
+            parent.spawn(SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgb(0.6, 0.6, 0.6),
+                    custom_size: Vec2::new(0.5, 0.8).into(),
+                    ..Default::default()
+                },
+                transform: Transform::from_translation(Vec3::new(0.0, 0.4, 0.0)),
+                ..Default::default()
+            });
+        })
+        .insert((
+            Archer {
+                id: 0,
+                aim_angle: 0.0,
+                bow_pull: 0.0,
+            },
+            TrajectoryReceiver,
+        ));
+}
+
+fn archer_controll(time: Res<Time>, keyboard: Res<Input<KeyCode>>, mut query: Query<&mut Archer>) {
+    for mut archer in query.iter_mut() {
+        if keyboard.pressed(KeyCode::Up) {
+            archer.aim_angle += 5.0 * time.delta_seconds();
+        }
+
+        if keyboard.pressed(KeyCode::Down) {
+            archer.aim_angle -= 5.0 * time.delta_seconds();
+        }
+
+        if archer.aim_angle <= -PI {
+            archer.aim_angle += 2.0 * PI;
+        }
+
+        if archer.aim_angle >= PI {
+            archer.aim_angle -= 2.0 * PI;
+        }
+
+        if keyboard.pressed(KeyCode::Left) {
+            archer.bow_pull += 2.0 * time.delta_seconds();
+        }
+
+        if keyboard.pressed(KeyCode::Right) {
+            archer.bow_pull -= 2.0 * time.delta_seconds();
+        }
+
+        archer.bow_pull = f32::clamp(archer.bow_pull, 0.0, 1.0);
+    }
+}
+
+fn archer_combat_limb_system(
+    mut query: Query<(&mut TargetRotation, &ArcherElement), With<CombatLimb>>,
+    query_archer: Query<&Archer>,
+) {
+    for (mut target_rotation, element) in query.iter_mut() {
+        let mut found_archer: Option<&Archer> = Option::None;
+        for archer in query_archer.iter() {
+            if archer.id == element.archer_id {
+                found_archer = Option::Some(archer);
+                break;
+            }
+        }
+        if let Some(archer) = found_archer {
+            target_rotation.angle = archer.aim_angle;
+        }
+    }
+}
+
+fn target_rotation_system(time: Res<Time>, mut query: Query<(&mut Transform, &TargetRotation)>) {
+    for (mut transform, target_rotation) in query.iter_mut() {
+        if !target_rotation.is_enabled {
+            continue;
+        }
+
+        let rot_z = transform.rotation.to_euler(EulerRot::XYZ).2;
+        let desired_rot_z = target_rotation.angle;
+        let mut new_rot = rot_z;
+        match target_rotation.rotation_type {
+            TargetRotationType::Instant => {
+                new_rot = desired_rot_z;
+            }
+            TargetRotationType::Smooth(speed) => {
+                let rot_delta = speed * time.delta_seconds();
+                let rot_diff = f32::abs(desired_rot_z - rot_z);
+                let rot_diff_comp = (2.0 * PI) - rot_diff;
+                if rot_diff < 0.01 {
+                    new_rot = desired_rot_z;
+                } else {
+                    let mut direction = if rot_z < desired_rot_z { 1.0 } else { -1.0 };
+                    if rot_diff_comp < rot_diff {
+                        direction *= -1.0;
+                    }
+
+                    new_rot += rot_delta * direction;
+                }
+            }
+        }
+
+        transform.rotation = Quat::from_euler(EulerRot::XYZ, 0.0, 0.0, new_rot);
+    }
+}
